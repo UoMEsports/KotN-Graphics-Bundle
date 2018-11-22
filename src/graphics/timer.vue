17abf8f8456b2e36d4362b8073b8e54b17194e13
Dan Shields <d.shieldsuk@gmail.com>
2018-11-22 01:54:44 +0000

    Timer layout

--- src/graphics/timer.vue
+++ src/graphics/timer.vue
@@ -1,6 +1,10 @@
 <template>
-	<div>
-		<h1 v-if="!hidden">{{time}}</h1>
+	<div v-if="!hidden" class="timer">
+		<ul class="list">
+			<li><h1 class="mins">{{mins}}</h1>
+			<li><h1 class="separator">:</h1>
+			<li><h1 class="secs">{{secs}}</h1>
+		</ul>
 		<hr class="line"/>
 		<h1 class="text">{{text}}</h1>
 	</div>
@@ -12,8 +16,9 @@ const timerRep = nodecg.Replicant('timer');
 export default {
 	data() {
 		return {
-			time: '00:00',
-			hidden: 'false'
+			mins: null,
+			secs: null,
+			hidden: 'false',
 			text: ''
 		};
 	},
@@ -24,10 +29,14 @@ export default {
 		listen() {
 			console.log("listening for timer changes");
 			timerRep.on('change', newVal => {
-				this.time = newVal.formatted;
+				this.mins = this.pad(newVal.minutes);
+				this.secs = this.pad(newVal.seconds);
 				this.hidden = newVal.hidden;
 				this.text = String(newVal.text).toUpperCase();
 			});
+		},
+		pad(num) {
+			return num < 10 ? `0${num}` : `${num}`;
 		}
 	}
 };
@@ -37,7 +46,55 @@ export default {
 @import url("https://use.typekit.net/mah1qnl.css");
 @import '../../shared/style/main.scss';
 
-h1 {
-	font-size: 360px;
+.timer {
+	position: absolute;
+	left: $width / 2;
+	width: 600px;
+	bottom: 26px;
+	transform: translateX(-50%);
+
+	* {
+		text-align: center;
+		margin: 0;
+		color: $primary;
+	}
+
+	.list {
+		list-style: none;
+		margin-bottom: 160px;
+		li {
+			h1 {
+				display: inline;
+				line-height: 1em;
+				font-size: 140px;
+				font-family: futura-pt, sans-serif;
+				position: absolute;
+			}
+
+			.mins {
+				right: 55%;
+			}			
+			.separator {
+				left: 50%;
+				transform: translateX(-50%);
+			}
+			.secs {
+				left: 55%;
+			}
+		}
+	}
+	.line {
+		margin: 10px auto;
+		border: none;
+    	height: 4px;
+		background-color: white;
+		width: 525px;
+	}
+	.text {
+		line-height: 1em;
+		font-size: 67px;
+		font-family: futura-pt, sans-serif;
+	}
 }
+
 </style>
